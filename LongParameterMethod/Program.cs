6699205be42a52956bf000169e5f4e6a87017ec9
Alexander Putilin <Marshall483@MacBook-Pro-Mac.local>
2021-05-11 20:00:00 +0300

    Initial

---
+++ LongParameterMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LongParameterMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
